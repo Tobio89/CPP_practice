8c1eb01fd0cea5524947d5ff7d69b84188a7fd6a
bruceji7 <revolverc24@gmail.com>
2021-02-25 10:21:41 +0900

    Update pointer practice for extra clarity

--- pointer_practice/point.cpp
+++ pointer_practice/point.cpp
@@ -2,28 +2,34 @@
 
 int main() {
 
-    std::string food = "pizza";
+    std::string food = "pizza"; // Init. food as 'pizza'
+    std::cout << "food:         " << food << std::endl; // What is food?
 
     std::string &foodref = food; // Basically, 'what does food point to? I point to it, too.'
+    std::cout << "foodref:      " << foodref << "              - Reference, an alias for food" << std::endl;
 
     std::string* foodpoint = &food; // Save the address food has
+    std::cout << "foodpoint:    " <<foodpoint  << "     - Pointer, holds the address for food\n" << std::endl;
 
-    std::cout << food << std::endl; // What is food?
-    std::cout << &food << std::endl; // What is food's address?
+    std::cout << "std::cout << &food:  " <<&food  << "     - &food tells you the memory address for a variable\n" << std::endl;
 
-    std::cout << foodref << std::endl; 
-    std::cout << foodpoint << std::endl;
-
-    std::cout << *foodpoint << std::endl; // Find out what's at the address foodpoint is a pointer to
-
-
+    std::cout << "Reassign food to 'burger'" << std::endl;
     food = "burger";
+    std::cout << food << " -  food updated" << std::endl;
 
-    std::cout << food << std::endl;
+    std::cout << "Reassign foodref to 'chips'" << std::endl;
+    foodref = "chips"; // You can reassign the thing a reference points to like this
+    std::cout << food << " -  food updated via foodref" << std::endl;
 
-    *foodpoint = "chips";
 
-    std::cout << food << std::endl;
+    std::cout << "Reassign foodpoint to 'chicken'" << std::endl;
+    *foodpoint = "chicken"; // You can reassign the thing a pointer points to like this
+    std::cout << food << " -  food updated via *foodpoint" << std::endl;
+
+
+
+
+
 
 
 }
